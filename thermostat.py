d699845159b2ab54e5eca1c123f487b225027abe
Bart van Deenen <info@vandeenensupport.com>
2020-02-15 16:07:40 +0100

    bottle, schedule

--- thermostat.py
+++ thermostat.py
@@ -1,7 +1,45 @@
 
-from flask import Flask
-app = Flask(__name__)
+import json, time, datetime
+import bottle
 
-@app.route('/')
-def hello_world():
-    return 'Hello that works World!'
+app = bottle.Bottle() # pylint: disable=no-member
+
+def interpret_schedule():
+    with open("schedule.json","r") as fptr:
+        schedule = json.load(fptr)
+    now = datetime.datetime.now()
+    weekday = now.weekday()
+    active = None
+    for s in schedule['schedules']:
+        if s['type'] == 'weekly' and weekday in s['valid']:
+            active = s['schedule']
+    if not active:
+        return schedule['Tdefault']
+
+    print(active)
+    hm = now.strftime("%H:%M")
+    Temp = None
+    for d in active:
+        if hm > d['t']:
+            print(d)
+            Temp = d['T']
+
+    return Temp if Temp else schedule['Tdefault']
+
+
+
+@app.post('/control')
+def control():
+    print(bottle.request.json)
+    setpoint = interpret_schedule()
+    return {"setpoint": setpoint}
+
+
+@app.get('/')
+def index():
+    return {"setpoint": setpoint}
+
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000, reloader=True)
